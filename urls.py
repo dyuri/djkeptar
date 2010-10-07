2b8d9278ae80e4c11bb6e91dd1f40fb7ad4b23d5
Gyuri Horak <dyuri@horak.hu>
2010-10-07 12:14:55 +0200

    kis css szepites

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,4 +10,7 @@ urlpatterns = patterns('',
     url(r'^list/(?P<path>.*)$', 'keptar.views.listdir', name='listdir'),
     url(r'^show/(?P<fname>.*)$', 'keptar.views.showfile', name='showfile'),
     url(r'^admin/', include(admin.site.urls)),
+
+    # media files
+    url(r'media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
